39f65c4c6951f10c743fd3453ad05d3f72f8147e
Stuart Mumford <stuart@mumford.me.uk>
2015-06-29 15:45:09 +0100

    remove astropy favicon

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ except ImportError:
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = ''
+html_favicon = ''
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
