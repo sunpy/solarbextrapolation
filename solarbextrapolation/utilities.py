c49cc970b656a63536b18afd20db4eda7c893722
unknown <alex_ian_hamilton@hotmail.com>
2015-08-11 13:43:48 +0100

    Added examples for extrapolating and visualising potential fields from HMI (using VSO) and example generated data. Added more documentation and various tweaks/fixes to a number of functions. Added the ability for extrapolators to crop/submap as defined by the user. Added units support (notably percentage positions and astropy quantity ranges) to the example data generator.

--- solarbextrapolation/utilities.py
+++ solarbextrapolation/utilities.py
@@ -20,10 +20,10 @@ def decompose_ang_len(qua_input, **kwargs):
     Parameters
     ----------
     
-    qua_input : astropy.quantity
+    qua_input : `astropy.units.quantity.Quantity`
         The quantity you wish decomposed.
     
-    working_units : astropy.unit, optional
+    working_units : `astropy.units.quantity.Quantity`, optional
         Unit that will be used for internal working and the returned quantity.
     Ensure that it is of the correct physical type, angle or length.
     
@@ -32,63 +32,67 @@ def decompose_ang_len(qua_input, **kwargs):
     
     """
     # Parameters
-    working_units = kwargs.get('working_units', u.m)
+    working_units = kwargs.get('working_units', u.m) * 1.0
     equivalence = kwargs.get('equivalencies', u.dimensionless_angles())
-
-    # Components of the quantity
-    value = qua_input.value
-    length_unit = 0.0 * u.m
-    length_exponent = 0.0
-    angle_unit = 0.0 * u.radian
-    angle_exponent = 0.0
     
-    # If we have at least 1 base, populate from the first base
-    if len(qua_input.unit.bases) > 0:
-        if qua_input.unit.bases[0].physical_type is u.m.physical_type:
-            length_unit = 1.0 * qua_input.unit.bases[0]
-            length_exponent = qua_input.unit.powers[0]
+    # Do nothing if the input is dimensionless.
+    if qua_input.unit is u.Quantity(1.0).unit:
+        return qua_input.decompose()
+    else:
+        # Components of the quantity
+        value = qua_input.value
+        length_unit = 0.0 * u.m
+        length_exponent = 0.0
+        angle_unit = 0.0 * u.radian
+        angle_exponent = 0.0
+        
+        # If we have at least 1 base, populate from the first base
+        if len(qua_input.unit.bases) > 0:
+            if qua_input.unit.bases[0].physical_type is u.m.physical_type:
+                length_unit     = 1.0 * qua_input.unit.bases[0]
+                length_exponent = qua_input.unit.powers[0]
+                
+                # convert to SI (meter here)
+                length_unit = length_unit.to(u.m)
+            elif qua_input.unit.bases[0].physical_type is u.radian.physical_type:
+                angle_unit     = 1.0 * qua_input.unit.bases[0]
+                angle_exponent = qua_input.unit.powers[0]
+                
+                # Convert to SI (radian here)
+                angle_unit = angle_unit.to(u.radian)
+        
+        # If we have 2 bases, populate from the second base
+        if len(qua_input.unit.bases) > 1:
+            if qua_input.unit.bases[1].physical_type is u.m.physical_type:
+                length_unit     = 1.0 * qua_input.unit.bases[1]
+                length_exponent = qua_input.unit.powers[1]
+                
+                # Convert to SI (meter here)
+                length_unit = length_unit.to(u.m)
+            elif qua_input.unit.bases[1].physical_type is u.radian.physical_type:
+                angle_unit     = 1.0 * qua_input.unit.bases[1]
+                angle_exponent = qua_input.unit.powers[1]
+                
+                # Convert to SI (radian here)
+                angle_unit = angle_unit.to(u.radian)
             
-            # convert to SI (meter here)
-            length_unit = length_unit.to(u.m)
-        elif qua_input.unit.bases[0].physical_type is u.radian.physical_type:
-            angle_unit = 1.0 * qua_input.unit.bases[0]
-            angle_exponent = qua_input.unit.powers[0]
+        # Convert the incompatible base to the working units using the equivilence
+        if working_units.unit.physical_type is u.m.physical_type:
+            angle_unit = angle_unit.to(working_units, equivalencies=equivalence)
             
-            # Convert to SI (radian here)
-            angle_unit = angle_unit.to(u.radian)
+            # Strip out the units, so the output doesn't have squared lenth units
+            #angle_unit = angle_unit.value # Kept in-case it causes bugs
+        elif working_units.unit.physical_type is u.radian.physical_type:
+            length_unit = length_unit.to(working_units, equivalencies=equivalence)
     
-    # If we have 2 bases, populate from the second base
-    if len(qua_input.unit.bases) > 1:
-        if qua_input.unit.bases[1].physical_type is u.m.physical_type:
-            length_unit = 1.0 * qua_input.unit.bases[1]
-            length_exponent = qua_input.unit.powers[1]
-            
-            # Convert to SI (meter here)
-            length_unit = length_unit.to(u.m)
-        elif qua_input.unit.bases[1].physical_type is u.radian.physical_type:
-            angle_unit = 1.0 * qua_input.unit.bases[1]
-            angle_exponent = qua_input.unit.powers[1]
-            
-            # Convert to SI (radian here)
-            angle_unit = angle_unit.to(u.radian)
-        
-    # Convert the incompatible base to the working units using the equivilence
-    if working_units.physical_type is u.m.physical_type:
-        angle_unit = angle_unit.to(working_units, equivalencies=equivalence)
-        
-        # Strip out the units, so the output doesn't have squared lenth units
-        #angle_unit = angle_unit.value # Kept in-case it causes bugs
-    elif working_units.physical_type is u.radian.physical_type:
-        length_unit = length_unit.to(working_units, equivalencies=equivalence)
-
-        # Strip out the units, so the output doesn't have squared length units
-        #length_unit = length_unit.value # Kept in-case it causes bugs
-    # The quantity to return
-    quantity =  value * length_unit ** length_exponent * angle_unit ** angle_exponent
-    # Change to the working unit if not dimensionless
-    if quantity.unit.physical_type is not (u.m / u.m).decompose().physical_type:
-        quantity.to(working_units)
-    return quantity.decompose()
+            # Strip out the units, so the output doesn't have squared length units
+            #length_unit = length_unit.value # Kept in-case it causes bugs
+        # The quantity to return
+        quantity =  value * length_unit ** length_exponent * angle_unit ** angle_exponent
+        # Change to the working unit if not dimensionless
+        if quantity.unit.physical_type is not (u.m / u.m).decompose().physical_type:
+            quantity.to(working_units)
+        return quantity.decompose()
     
 
 def si_this_map_OLD(map):
