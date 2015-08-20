d57badd85fcfad447b16ead3d09916543373df7f
Alex-Ian-Hamilton <alex_ian_hamilton@hotmail.com>
2015-08-20 12:57:03 +0100

    Removed __name__ '__main__' from example files.

--- examples/example_generate_example_data.py
+++ examples/example_generate_example_data.py
@@ -18,31 +18,30 @@ from solarbextrapolation.utilities import *
 from solarbextrapolation.example_data_generator import *
 from solarbextrapolation.visualisation_functions import *
 
-if __name__ == '__main__':
-    # Generate an example map
-    # The input parameters:
-    arr_grid_shape = [ 20, 22, 20 ]         # [ y-size, x-size ]
-    xrange = u.Quantity([ -10.0, 10.0 ] * u.arcsec)
-    yrange = u.Quantity([ -11.0, 11.0 ] * u.arcsec)
-    zrange = u.Quantity([ 0,     20.0 ] * u.arcsec)
+# The input parameters:
+arr_grid_shape = [ 20, 22, 20 ]         # [ y-size, x-size ]
+xrange = u.Quantity([ -10.0, 10.0 ] * u.arcsec)
+yrange = u.Quantity([ -11.0, 11.0 ] * u.arcsec)
+zrange = u.Quantity([ 0,     20.0 ] * u.arcsec)
     
-    # Manual Pole Details
-    arrA0 = [ u.Quantity([ 25, 25 ] * u.percent), 10.0 * u.percent,  0.2 * u.T ]
-    arrA1 = [ u.Quantity([ 75, 75 ] * u.percent), 10.0 * u.percent, -0.2 * u.T ]
+# Manual Pole Details
+arrA0 = [ u.Quantity([ 25, 25 ] * u.percent), 10.0 * u.percent,  0.2 * u.T ]
+arrA1 = [ u.Quantity([ 75, 75 ] * u.percent), 10.0 * u.percent, -0.2 * u.T ]
 
-    # Generate the data and save to a map
-    arr_data = generate_example_data(arr_grid_shape[0:2], xrange, yrange, arrA0, arrA1)
-    map_boundary = dummyDataToMap(arr_data, xrange, yrange)
+# Generate the data and save to a map
+arr_data = generate_example_data(arr_grid_shape[0:2], xrange, yrange, arrA0, arrA1)
+map_boundary = dummyDataToMap(arr_data, xrange, yrange)
     
-    # Use potential extrapolator to generate field
-    aPotExt = PotentialExtrapolator(map_boundary, zshape=arr_grid_shape[2], zrange=zrange)
-    aMap3D  = aPotExt.extrapolate()
-    
-    # Visualise
-    visualise(aMap3D,
-              boundary=map_boundary,
-              volume_units=[1.0*u.arcsec, 1.0*u.arcsec, 1.0*u.Mm],
-              show_boundary_axes=False,
-              boundary_units=[1.0*u.arcsec, 1.0*u.arcsec],
-              show_volume_axes=True,
-              debug=False)
+# Use potential extrapolator to generate field
+aPotExt = PotentialExtrapolator(map_boundary, zshape=arr_grid_shape[2], zrange=zrange)
+aMap3D  = aPotExt.extrapolate()
+print aMap3D.meta['extrapolator_duration']
+
+# Visualise
+visualise(aMap3D,
+          boundary=map_boundary,
+          volume_units=[1.0*u.arcsec, 1.0*u.arcsec, 1.0*u.Mm],
+          show_boundary_axes=False,
+          boundary_units=[1.0*u.arcsec, 1.0*u.arcsec],
+          show_volume_axes=True,
+          debug=False)
