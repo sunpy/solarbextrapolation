b5c09199219ee6518e1957b7610f9e8622716d35
Stuart Mumford <stuart@cadair.com>
2017-08-15 21:46:16 +0100

    try again

--- docs/conf.py
+++ docs/conf.py
@@ -134,10 +134,7 @@ release = package.__version__
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
-try:
-    from sunpy_sphinx_theme.conf import *
-except ImportError:
-    html_theme = 'default'
+from sunpy_sphinx_theme.conf import *
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
 
